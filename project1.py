3762d6ad2251928d1395c489d854138949758744
Ronjal12 <adhikarironjal@gmail.com>
2025-06-07 18:37:13 +0545

    Update project1.py

--- project1.py
+++ project1.py
@@ -1,40 +1,50 @@
 import random
-L=['snake','water','gun']
 
-computer=random.choice(L)
+def func1():
+    L=['snake','water','gun']
 
-print(L)
+    computer=random.choice(L)
 
-your_choice=int(input("Enter your choice in number"))
+    print(L)
 
-Your_choice=L[your_choice-1]
+    your_choice=int(input("Enter your choice in number"))
 
-
-if(Your_choice==computer):
-    print("Its a draw.")
-    print(f"computer chose {computer}")
+    Your_choice=L[your_choice-1]
 
 
-elif(Your_choice=="snake" and computer=="water"):
-    print("You win.")
-    print(f"computer chose {computer}")
+    if(Your_choice==computer):
+        print("Its a draw.")
+        print(f"computer chose {computer}")
 
-elif(Your_choice=="snake" and computer=="gun"):
-    print("You lose.")
-    print(f"computer chose {computer}")
 
-elif(Your_choice=="water" and computer=="snake"):
-    print("You lose.")
-    print(f"computer chose {computer}")
+    elif(Your_choice=="snake" and computer=="water"):
+        print("You win.")
+        print(f"computer chose {computer}")
 
-elif(Your_choice=="water" and computer=="gun"):
-    print("You win.")
-    print(f"computer chose {computer}")
+    elif(Your_choice=="snake" and computer=="gun"):
+        print("You lose.")
+        print(f"computer chose {computer}")
 
-elif(Your_choice=="gun" and computer=="snake"):
-    print("You win.")
-    print(f"computer chose {computer}")
+    elif(Your_choice=="water" and computer=="snake"):
+        print("You lose.")
+        print(f"computer chose {computer}")
 
-elif(Your_choice=="gun" and computer=="water"):
-    print("You lose.")
-    print(f"computer chose {computer}")
+    elif(Your_choice=="water" and computer=="gun"):
+        print("You win.")
+        print(f"computer chose {computer}")
+
+    elif(Your_choice=="gun" and computer=="snake"):
+        print("You win.")
+        print(f"computer chose {computer}")
+
+    elif(Your_choice=="gun" and computer=="water"):
+        print("You lose.")
+        print(f"computer chose {computer}")
+
+choice="y"
+
+while(choice =="y"):
+    func1()
+    choice=input("DO you want to continue playing y/n").lower() 
+
+print("Thankyou for playing.")
