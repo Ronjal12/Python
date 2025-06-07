e8199dc24c0c01f0f80d4e4801d36130758db2e3
Ronjal12 <adhikarironjal@gmail.com>
2025-06-07 18:15:50 +0545

    add this

---
+++ project1.py
@@ -0,0 +1,40 @@
+import random
+L=['snake','water','gun']
+
+computer=random.choice(L)
+
+print(L)
+
+your_choice=int(input("Enter your choice in number"))
+
+Your_choice=L[your_choice-1]
+
+
+if(Your_choice==computer):
+    print("Its a draw.")
+    print(f"computer chose {computer}")
+
+
+elif(Your_choice=="snake" and computer=="water"):
+    print("You win.")
+    print(f"computer chose {computer}")
+
+elif(Your_choice=="snake" and computer=="gun"):
+    print("You lose.")
+    print(f"computer chose {computer}")
+
+elif(Your_choice=="water" and computer=="snake"):
+    print("You lose.")
+    print(f"computer chose {computer}")
+
+elif(Your_choice=="water" and computer=="gun"):
+    print("You win.")
+    print(f"computer chose {computer}")
+
+elif(Your_choice=="gun" and computer=="snake"):
+    print("You win.")
+    print(f"computer chose {computer}")
+
+elif(Your_choice=="gun" and computer=="water"):
+    print("You lose.")
+    print(f"computer chose {computer}")
