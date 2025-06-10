33f37d144147ed8a3b2481ff807b2629aa67b35e
Ronjal12 <adhikarironjal@gmail.com>
2025-06-10 14:59:10 +0545

    w

--- table/tables.py
+++ table/tables.py
@@ -3,7 +3,7 @@ def generateTable(n):
     for i in range(1,11):
         table+=f"{n} X {i} = {n*i}"
 
-    with open(f"table/table_{n}", "w") as f:
+    with open(f"table", "w") as f:
         f.write(table)
         
 for i in range(2,21):
