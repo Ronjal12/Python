9105e00efa9f67ddb2f402406fa26c631a5ddd77
Ronjal12 <adhikarironjal@gmail.com>
2025-06-16 14:10:10 +0545

    c

---
+++ class.py
@@ -0,0 +1,8 @@
+class programmer:
+    company="Microsoft"
+    def __init__(self,name,salary,pin):
+        self.name=name
+        self.salary=salary
+        self.pin=pin
+
+p=programmer("ronjal",12000,9865)
